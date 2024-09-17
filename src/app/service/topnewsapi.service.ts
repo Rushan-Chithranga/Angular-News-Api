9dfe22ea3e26558a311a35701e9ddf4ac869b0ed
Rushan Chithranga <rushanchith123@gmail.com>
2024-09-17 16:01:39 +0530

    setup proxy

--- src/app/service/topnewsapi.service.ts
+++ src/app/service/topnewsapi.service.ts
@@ -9,7 +9,7 @@ export class TopnewsapiService {
   constructor(private _http: HttpClient) {}
 
   topHeadlinesNewsUrl =
-    'https://newsapi.org/v2/top-headlines?country=us&apiKey=7e9e34c530c440bf9dec9b98cf53cc60';
+    '/newsapi/v2/top-headlines?country=us&apiKey=7e9e34c530c440bf9dec9b98cf53cc60';
 
   tcHeadLines(): Observable<any> {
     return this._http.get(this.topHeadlinesNewsUrl);
